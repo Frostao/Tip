befd55a143027f77b5a658fe9b70f24c4057d8b5
Carl Chen <chen1725@purdue.edu>
2015-12-07 11:02:10 -0500

    update

--- Tip/AppDelegate.swift
+++ Tip/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        //init some apperance
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.blueColor()]
+        
+        
         // Override point for customization after application launch.
         return true
     }
